1ada3c462b136cd800775b2db4a250ece104eadb
benjamin <b.rondo@orange.fr>
2024-02-13 11:13:16 +0100

    test

--- js/game.js
+++ js/game.js
@@ -4,14 +4,14 @@ let buildingsData, resourcesData, playerData;
 
 async function loadGameData() {
     try {
-        const buildingsResponse = await fetch('../../data/woodbuild.json');
+        const buildingsResponse = await fetch('../data/woodbuild.json');
         buildingsData = await buildingsResponse.json();
 
-        const resourcesResponse = await fetch('../../data/ressources.json');
+        const resourcesResponse = await fetch('../data/ressources.json');
         const resourcesRawData = await resourcesResponse.json();
         resourcesData = resourcesRawData.woods; // Accédez à la clé "woods" du JSON
 
-        const playerResponse = await fetch('../../data/player.json');
+        const playerResponse = await fetch('../data/player.json');
         playerData = await playerResponse.json();
 
         console.log('Données du jeu chargées:', buildingsData, resourcesData, playerData);
