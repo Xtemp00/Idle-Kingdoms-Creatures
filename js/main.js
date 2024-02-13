f6998e23a33083f8517560965cef5a52d12b8093
RyanLru <ryanloureau@gmail.com>
2024-02-13 13:15:01 +0100

    Add click event listener to wiki button

--- js/main.js
+++ js/main.js
@@ -10,6 +10,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function attachMenuListeners() {
     var playButton = document.getElementById('play-button');
+    var wikiButton = document.getElementById('wiki-button');
+    wikiButton.addEventListener('click', function() {
+        // Ouverture de wiki/wiki.html dans la même fenêtre
+        window.location.href = 'wiki/wiki.html';
+    });
     playButton.addEventListener('click', function() {
         // Afficher game-container
         document.getElementById('game-container').style.display = 'block';
