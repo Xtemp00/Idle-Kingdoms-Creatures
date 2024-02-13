5aa5f69db677669e0fca7677c588ee5aabdc153a
benjamin <b.rondo@orange.fr>
2024-02-13 18:49:47 +0100

    Add wood minimap and toggle functionality

--- js/ui-interactions.js
+++ js/ui-interactions.js
@@ -3,6 +3,7 @@ function toggleSections(sectionToShow) {
     const citymap = document.getElementById('game-citymap');
     const woodSection = document.getElementById('game-part-wood');
     const woodXp = document.getElementById('wood-xp-progress-bar');
+    const minimap = document.getElementById('wood-minimap');
     //const mineSection = document.getElementById('mine-section');
 
     // Masquer la citymap
@@ -10,15 +11,21 @@ function toggleSections(sectionToShow) {
 
     // Masquer toutes les sections
     woodSection.style.display = 'none';
+    woodXp.style.display = 'none';
     //mineSection.style.display = 'none';
 
     // Afficher la section demandée
     if (sectionToShow === 'wood') {
         woodSection.style.display = 'grid';
         woodXp.style.display = 'flex';
+        minimap.style.display = 'flex';
     } /*else if (sectionToShow === 'mine') {
         mineSection.style.display = 'block';
     }*/
+    else if (sectionToShow === 'citymap') {
+        citymap.style.display = 'flex';
+        minimap.style.display = 'none';
+    }
 }
 
 // Ajouter des écouteurs d'événements aux boutons
@@ -28,4 +35,11 @@ document.getElementById('wood-button').addEventListener('click', function() {
 
 document.getElementById('mine-button').addEventListener('click', function() {
     toggleSections('mine');
+});
+
+document.getElementById('minimap-button').addEventListener('click', function() {
+    // Votre logique pour retourner a la city
+    // Par exemple, afficher le menu ou changer de page
+    toggleSections('citymap');
+
 });
