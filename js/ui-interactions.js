af54814fc24e759078e676a55d23588d791c635c
benjamin <b.rondo@orange.fr>
2024-02-13 16:25:38 +0100

    Add citymap buttons and styles

---
+++ js/ui-interactions.js
@@ -0,0 +1,31 @@
+// Fonction pour afficher/masquer les sections
+function toggleSections(sectionToShow) {
+    const citymap = document.getElementById('game-citymap');
+    const woodSection = document.getElementById('game-part-wood');
+    const woodXp = document.getElementById('wood-xp-progress');
+    //const mineSection = document.getElementById('mine-section');
+
+    // Masquer la citymap
+    citymap.style.display = 'none';
+
+    // Masquer toutes les sections
+    woodSection.style.display = 'none';
+    //mineSection.style.display = 'none';
+
+    // Afficher la section demandée
+    if (sectionToShow === 'wood') {
+        woodSection.style.display = 'flex';
+        woodXp.style.display = 'block';
+    } /*else if (sectionToShow === 'mine') {
+        mineSection.style.display = 'block';
+    }*/
+}
+
+// Ajouter des écouteurs d'événements aux boutons
+document.getElementById('wood-button').addEventListener('click', function() {
+    toggleSections('wood');
+});
+
+document.getElementById('mine-button').addEventListener('click', function() {
+    toggleSections('mine');
+});
